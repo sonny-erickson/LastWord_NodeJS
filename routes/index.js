88703cb042ecf2e09f4fe826c152f8bd527caa44
Sonny <pet63910@hotmail.fr>
2020-11-10 09:35:56 +0100

    Read Words collection of data

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const Word = require('../database/models/word.model');
 
 router.use('/api', api);
 //Définir les routes ci-dessous
+router.get('/wordForm', (req, res) =>{
+    res.render('../views/wordForm')
+})
 
 router.get('/', (req, res) =>{
     Word.find({})
@@ -12,12 +15,7 @@ router.get('/', (req, res) =>{
         .then( words => res.render('home', {words}))
 })
 
-// router.get('/', (req, res) =>{
-// res.render('../views/home')
-// })
-router.get('/wordForm', (req, res) =>{
-    res.render('../views/wordForm')
-})
+
 
 
 module.exports = router; //Pour exporter le router
